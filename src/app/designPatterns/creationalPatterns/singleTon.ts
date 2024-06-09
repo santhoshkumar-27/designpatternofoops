e443ac560762cfa3905d318a380246276d7daa95
santhoshkumar <its.santhoshkumar@outlook.com>
2024-06-09 12:59:59 +0530

    feat: design scrabble lsit

--- src/app/designPatterns/creationalPatterns/singleTon.ts
+++ src/app/designPatterns/creationalPatterns/singleTon.ts
@@ -5,6 +5,37 @@
 class Singleton {
     static #instance: Singleton;
 
+    // hold All my scrabble letters in these array
+    private scrabbleLetters: string[] = [
+        "A", "A", "A", "A", "A", "A", "A", "A", "A",
+        "B", "B",
+        "C", "C",
+        "D", "D", "D", "D",
+        "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E",
+        "F", "F",
+        "G", "G", "G",
+        "H", "H",
+        "I", "I", "I", "I", "I", "I", "I", "I", "I",
+        "J",
+        "K",
+        "L", "L", "L", "L",
+        "M", "M",
+        "N", "N", "N", "N", "N", "N",
+        "O", "O", "O", "O", "O", "O", "O", "O",
+        "P", "P",
+        "Q",
+        "R", "R", "R", "R", "R", "R",
+        "S", "S", "S", "S",
+        "T", "T", "T", "T", "T", "T",
+        "U", "U", "U", "U",
+        "V", "V",
+        "W", "W",
+        "X",
+        "Y", "Y",
+        "Z",
+        "", ""
+    ]
+
     /**
      * The Singleton's constructor should always be private to prevent direct
      * construction calls with the `new` operator.
@@ -29,8 +60,9 @@ class Singleton {
      * Finally, any singleton can define some business logic, which can be
      * executed on its instance.
      */
-    public someBusinessLogic() {
-        // ...
+
+    public getLetterList() {
+        return this.scrabbleLetters
     }
 }
 
