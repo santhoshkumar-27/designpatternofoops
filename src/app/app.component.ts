f4d4b0766e047d612e5e3a2caf8374c44412b824
santhoshkumar <its.santhoshkumar@outlook.com>
2024-06-23 12:49:30 +0530

    feat: convert strategy pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import ObservorPattern from './designPatterns/BehaviouralPatterns/observerpattern';
 import abstractFactory from './designPatterns/creationalPatterns/abstractFactoryPattern';
 import factoryPattern from './designPatterns/creationalPatterns/factorypattern';
-import strategyPattern from './designPatterns/creationalPatterns/strategypattern';
+import strategyPattern from './designPatterns/BehaviouralPatterns/strategypattern';
 import singleton from './designPatterns/creationalPatterns/singleTon';
 import builderPattern from './designPatterns/creationalPatterns/builderPattern';
 import { protoTypePattern } from './designPatterns/creationalPatterns/prototypePattern';
@@ -22,6 +22,10 @@ export class AppComponent {
     // singleton();
     // builderPattern();
     protoTypePattern();
+
+
+    // Structural pattern
+
 
     // Behavioural Patterns
     // ObservorPattern();
